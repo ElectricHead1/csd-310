1de7a39b9ed926effff740a92deb99d06e01e5d0
PaaTay2.0 <macbookpro@macbook-pro.myfiosgateway.com>
2021-04-12 00:45:30 -0400

    first commit

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,15 @@
+
+
+
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.4ysrd.mongodb.net/pytech?authSource=admin&replicaSet=atlas-115hgx-shard-0&readPreference=primary&ssl=true"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+print("\n -- Pytech Collection List --")
+print(db.list_collection_names())
+
+input("\n\n  End of program, press any key to exit... ")
