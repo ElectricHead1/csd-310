0b6ca0b6fb063e03b0c43759927949043f0cbb75
PaaTay2.0 <macbookpro@macbook-pro.myfiosgateway.com>
2021-05-09 20:54:21 -0400

    eighth commit

---
+++ module_10/whatabook_init.sql
@@ -0,0 +1,58 @@
+INSERT INTO store(locale)
+    VALUES('123 Main st. Philadelphia, PA 19023');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Return of the King', 'J.R.Tolkien', 'The third part of The Lord of the Rings');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Fellowship of the Ring', 'J.R.Tolkien', 'The first part of The Lord of the Rings');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Two Towers', 'J.R.Tolkien', "The second part of The Lord of The Rings");
+
+INSERT INTO book(book_name, author)
+    VALUES('The Hobbit or There and Back Again', 'J.R.Tolkien');
+
+INSERT INTO book(book_name, author)
+    VALUES('Dune: Deluxe Edition', 'Frank Herbert');
+
+INSERT INTO book(book_name, author)
+    VALUES("Charlotee's Web", 'E.B. White');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Great Gatsby', 'F. Scott Fitzgerald');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Lion, the Witch, and the Wardrobe', 'C.S. Lewis');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Catcher and the Rye', 'J.D. Salinger');
+
+
+INSERT INTO user(first_name, last_name) 
+    VALUES('Thorin', 'Oakenshield');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Bilbo', 'Baggins');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Frodo', 'Baggins');
+
+
+INSERT INTO wishlist(user_id, book_id) 
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Thorin'), 
+        (SELECT book_id FROM book WHERE book_name = 'The Hobbit or There and Back Again')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Bilbo'),
+        (SELECT book_id FROM book WHERE book_name = 'The Fellowship of the Ring')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Frodo'),
+        (SELECT book_id FROM book WHERE book_name = 'The Return of the King')
+    );
